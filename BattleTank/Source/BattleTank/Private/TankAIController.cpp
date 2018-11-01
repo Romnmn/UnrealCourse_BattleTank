9a684c5aa9b04c041d894a046c6ad78549a8e6db
Roman Daragan <daragan.rom@gmail.com>
2018-11-01 23:36:12 +0200

    AI controller class

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,6 +2,24 @@
 
 #include "TankAIController.h"
 
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI C is not possesing a tank"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("AI C is possesing a tank : %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank * ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
 
 
