f15eba16d4e04c397fdd2c998e050720a4acebc5
Roman Daragan <daragan.rom@gmail.com>
2018-11-18 15:03:48 +0200

    Barrel is moving

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -7,6 +7,13 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 	//auto Time = GetWorld()->GetTimeSeconds();
 
 	//UE_LOG(LogTemp, Warning, TEXT("%f Barrel Elevate() is called, D perS is %f"),Time, RelativeSpeed);
+
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+
+	SetRelativeRotation(FRotator(Elevation, 0, 0));
 }
 
 
--- BattleTank/Source/BattleTank/Public/TankBarrel.h
+++ BattleTank/Source/BattleTank/Public/TankBarrel.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
 #include "TankBarrel.generated.h"
 
 /**
@@ -19,7 +20,7 @@ public:
 	void Elevate(float RelativeSpeed);
 	
 private:
-	UPROPERTY(EditAnywhere, Category = Setup) float MaxDegreesPerSecond = 20;
+	UPROPERTY(EditAnywhere, Category = Setup) float MaxDegreesPerSecond = 5;
 
 	UPROPERTY(EditAnywhere, Category = Setup) float MinElevationDegrees = 0;
 
