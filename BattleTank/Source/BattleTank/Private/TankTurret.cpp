d8cd3f5d4d561e8c81911bb3b34a80813f5c1ecb
Roman Daragan <daragan.rom@gmail.com>
2018-12-14 01:12:24 +0200

    Progress saving

--- BattleTank/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankTurret.h"
+#include "Engine/World.h"
 
 void UTankTurret::SetTurretMesh(UStaticMesh * MeshToSet)
 {
@@ -13,8 +14,7 @@ void UTankTurret::Rotate(float RelativeSpeed)
 {
 	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
 	auto RotationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	auto RawNewRotation = RelativeRotation.Yaw + RotationChange;
-	auto Rotation = FMath::Clamp<float>(RawNewRotation, MinRotationDegrees, MaxRotationDegrees);
+	auto Rotation = RelativeRotation.Yaw + RotationChange;
 
 	SetRelativeRotation(FRotator(0, Rotation, 0));
 }
